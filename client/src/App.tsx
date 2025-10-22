3ed221d06774b4b04524976f6fe95d8b87ae2647
Eugenio Fernandez <efdeugenio@gmail.com>
2025-10-22 17:25:38 -0600

    Revert "Merge pull request #13 "
    This reverts commit ee1a32c98b58d28ea8c1edead1ba5cd6d5c743d5, reversing
    changes made to d5dd69fc06a2c4b22d67b7008a0cc079e84b4c27.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,15 +5,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
-import SuccessPage from "@/pages/success";
-import CancelPage from "@/pages/cancel";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/success" component={SuccessPage} />
-      <Route path="/cancel" component={CancelPage} />
       <Route component={NotFound} />
     </Switch>
   );
